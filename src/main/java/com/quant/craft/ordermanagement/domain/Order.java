3d7fd02a8b8c4b48654139ceeda8820111577306
ehdtndla123 <ehdtndla123@naver.com>
2024-08-30 17:23:54 +0900

    feat : 각종 enum 추가 및 조건부 주문 추가

--- src/main/java/com/quant/craft/ordermanagement/domain/Order.java
+++ src/main/java/com/quant/craft/ordermanagement/domain/Order.java
@@ -11,6 +11,8 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "orders")
@@ -21,7 +23,7 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
+    private Long id;
 
     @Column(unique = true, nullable = false)
     private String orderId;
@@ -31,58 +33,72 @@ public class Order {
     private String symbol;
     private String exchange;
     private BigDecimal size;
-    private BigDecimal limit;
-    private BigDecimal stop;
-    private BigDecimal sl;
-    private BigDecimal tp;
-    private BigDecimal leverage;
+    private BigDecimal price;
+    private int leverage;
 
+    // 실질적으로 고객에게 보여지는 주문상태
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
+    // 주문 유형 Market LIMIT 등등
     @Enumerated(EnumType.STRING)
     private OrderType type;
 
+    // LONG , SHORT
     @Enumerated(EnumType.STRING)
-    private OrderPosition position;
+    private TradeDirection direction;
+
+    // OPEN , CLOSE
+    @Enumerated(EnumType.STRING)
+    private OrderAction action;
+
+    // 진행상태를 위해 인위적으로 만든 Status
+    @Enumerated(EnumType.STRING)
+    private ProcessingStatus processingStatus;
+
+    @OneToMany(mappedBy = "parentOrder", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ConditionalOrder> conditionalOrders = new ArrayList<>();
 
     @CreatedDate
     private LocalDateTime createdAt;
+
     @LastModifiedDate
     private LocalDateTime updatedAt;
 
     @Builder
     public Order(String orderId, Long botId, Long tradingBotId, String symbol, String exchange,
-                 BigDecimal size, BigDecimal limit, BigDecimal stop, BigDecimal sl, BigDecimal tp, BigDecimal leverage,
-                 OrderType type, OrderPosition position, OrderStatus status) {
+                 BigDecimal size, BigDecimal price, int leverage,
+                 OrderType type, OrderStatus status, TradeDirection direction, OrderAction action, ProcessingStatus processingStatus) {
         this.orderId = orderId;
         this.botId = botId;
         this.tradingBotId = tradingBotId;
         this.symbol = symbol;
         this.exchange = exchange;
         this.size = size;
-        this.limit = limit;
-        this.stop = stop;
-        this.sl = sl;
-        this.tp = tp;
+        this.price = price;
         this.leverage = leverage;
         this.type = type;
-        this.position = position;
-        setStatus(status);
-    }
-
-    private void setStatus(OrderStatus status) {
-        if (status == null || !status.isActive()) {
-            throw new IllegalArgumentException("Initial order status must be OPEN or PARTIALLY_FILLED");
-        }
+        this.direction = direction;
         this.status = status;
+        this.action = action;
+        this.processingStatus = processingStatus;
     }
 
-    public void updateStatus(OrderStatus newStatus) {
-        if (newStatus == null || !newStatus.isActive()) {
-            throw new IllegalArgumentException("Updated order status must be OPEN or PARTIALLY_FILLED");
-        }
-        this.status = newStatus;
+    public void addConditionalOrder(ConditionalOrder conditionalOrder) {
+        conditionalOrders.add(conditionalOrder);
+        conditionalOrder.setParentOrder(this);
     }
 
+    public void removeConditionalOrder(ConditionalOrder conditionalOrder) {
+        conditionalOrders.remove(conditionalOrder);
+        conditionalOrder.setParentOrder(null);
+    }
+
+    public void setProcessingStatus(ProcessingStatus processingStatus) {
+        this.processingStatus = processingStatus;
+    }
+
+    public void setStatus(OrderStatus orderStatus) {
+        this.status = orderStatus;
+    }
 }
