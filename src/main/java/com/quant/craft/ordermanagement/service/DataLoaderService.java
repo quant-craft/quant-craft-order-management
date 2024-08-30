694a9f1fd2239c99c929a7c4db3ea1702f9a0146
ehdtndla123 <ehdtndla123@naver.com>
2024-08-31 03:00:34 +0900

    feat : OHLCVData 최근데이터를 다루는 DataLoaderService 추가

---
+++ src/main/java/com/quant/craft/ordermanagement/service/DataLoaderService.java
@@ -0,0 +1,27 @@
+package com.quant.craft.ordermanagement.service;
+import com.quant.craft.ordermanagement.dto.OHLCVData;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Service
+public class DataLoaderService {
+
+    private final ConcurrentMap<String, OHLCVData> latestOHLCVData = new ConcurrentHashMap<>();
+
+    public void processOHLCVData(OHLCVData data) {
+        String key = getKey(data.getExchange(), data.getSymbol());
+        latestOHLCVData.put(key, data);
+    }
+
+    public OHLCVData getLatestOHLCVData(String exchange, String symbol) {
+        String key = getKey(exchange, symbol);
+        return latestOHLCVData.get(key);
+    }
+
+    private String getKey(String exchange, String symbol) {
+        return exchange + ":" + symbol;
+    }
+
+}
