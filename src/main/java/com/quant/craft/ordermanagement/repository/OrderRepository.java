d6617938ae2d51d82d9dcda527e4c123ea594795
ehdtndla123 <ehdtndla123@naver.com>
2024-09-02 22:25:37 +0900

    refactor : OrderStatus None -> OPEN 변경

--- src/main/java/com/quant/craft/ordermanagement/repository/OrderRepository.java
+++ src/main/java/com/quant/craft/ordermanagement/repository/OrderRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    @Query("SELECT o FROM Order o WHERE o.botId = :botId AND o.symbol = :symbol AND o.status IN (com.quant.craft.ordermanagement.domain.OrderStatus.NONE, com.quant.craft.ordermanagement.domain.OrderStatus.PARTIALLY_FILLED)")
+    @Query("SELECT o FROM Order o WHERE o.botId = :botId AND o.symbol = :symbol AND o.status IN (com.quant.craft.ordermanagement.domain.OrderStatus.OPEN, com.quant.craft.ordermanagement.domain.OrderStatus.PARTIALLY_FILLED)")
     List<Order> findOpenOrdersByBotIdAndSymbol(
             @Param("botId") Long botId,
             @Param("symbol") String symbol);
