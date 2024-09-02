d5205ddbe713666be1403bac53eb3779a5ac50fc
ehdtndla123 <ehdtndla123@naver.com>
2024-09-02 22:33:14 +0900

    feat : JsonProperty 추가

--- src/main/java/com/quant/craft/ordermanagement/dto/OrderDto.java
+++ src/main/java/com/quant/craft/ordermanagement/dto/OrderDto.java
@@ -1,5 +1,6 @@
 package com.quant.craft.ordermanagement.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
@@ -9,10 +10,13 @@ import java.math.BigDecimal;
 public class OrderDto {
 
     @NotNull
+    @JsonProperty("order_id")
     private String orderId;
     @NotNull
+    @JsonProperty("bot_id")
     private Long botId;
     @NotNull
+    @JsonProperty("trading_bot_id")
     private Long tradingBotId;
     @NotNull
     private String symbol;
@@ -25,7 +29,9 @@ public class OrderDto {
     private BigDecimal sl;
     private BigDecimal tp;
     private int leverage;
+    @JsonProperty("exclusive_orders")
     private Boolean exclusiveOrders;
+    @JsonProperty("hedge_mode")
     private Boolean hedgeMode;
 
     // 추후 변경 Config에서 지원하는 Exchange 가져오도록.
