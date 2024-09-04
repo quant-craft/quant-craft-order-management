afe6f0b4d26e108e387444cdc317db7f5d23a0c1
ehdtndla123 <ehdtndla123@naver.com>
2024-09-04 15:50:29 +0900

    refactor : Order에 대한 Response, Trade에 대한 Response 분리

--- src/main/java/com/quant/craft/ordermanagement/EMS/OrderRequestEventListener.java
+++ src/main/java/com/quant/craft/ordermanagement/EMS/OrderRequestEventListener.java
@@ -3,6 +3,7 @@ package com.quant.craft.ordermanagement.EMS;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -10,6 +11,10 @@ public class OrderRequestEventListener {
 
     private final ExecutionManagementService executionManagementService;
 
+    /**
+     * TODO
+     * 추후 EMS 서버로 분리해야함 임시적인 메소드
+     */
     @EventListener
     public void handleOrderRequestEvent(OrderRequestEvent event) {
         executionManagementService.executeOrder(event.getOrder(),event.getType());
